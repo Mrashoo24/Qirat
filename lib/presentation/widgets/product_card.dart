76315d2823876e4cad90d340aa3ababed5ef67bf
Mannankha9 <mannan.khan@rozana.in>
2024-10-02 03:37:07 +0530

    ProductCard Changed

--- lib/presentation/widgets/product_card.dart
+++ lib/presentation/widgets/product_card.dart
@@ -9,6 +9,7 @@ class ProductCard extends StatelessWidget {
   final Product? product;
   final Function? onFavoriteToggle;
   final Function? onClick;
+
   const ProductCard({
     Key? key,
     this.product,
@@ -20,10 +21,10 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return product == null
         ? Shimmer.fromColors(
-            baseColor: Colors.grey.shade100,
-            highlightColor: Colors.white,
-            child: buildBody(context),
-          )
+      baseColor: Colors.grey.shade100,
+      highlightColor: Colors.white,
+      child: buildBody(context),
+    )
         : buildBody(context);
   }
 
@@ -35,110 +36,107 @@ class ProductCard extends StatelessWidget {
               .pushNamed(AppRouter.productDetails, arguments: product);
         }
       },
-      child: Container(
-        // width: 100,
-        height: MediaQuery.of(context).size.height * 0.3 ,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final width = constraints.maxWidth; // Get the available width
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: width,
+                height: width, // Ensure height is equal to the width
                 child: Ink(
-              decoration: BoxDecoration(
-                // color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Theme.of(context).shadowColor.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 8,
-                    offset: const Offset(0, 1),
-                  ),
-                ],
-                border: Border.all(
-                  color: Theme.of(context).shadowColor.withOpacity(0.15),
-                ),
-              ),
-              // Card(
-              //   color: Colors.white,
-              //   elevation: 0,
-              //   margin: const EdgeInsets.all(4),
-              //   clipBehavior: Clip.antiAlias,
-              //   shape: RoundedRectangleBorder(
-              //     borderRadius: BorderRadius.circular(12),
-              //   ),
-              child: product == null
-                  ? Material(
-                      child: GridTile(
-                        footer: Container(),
-                        child: Padding(
-                          padding: const EdgeInsets.all(24.0),
-                          child: Container(
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).shadowColor.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: const Offset(0, 1),
                       ),
-                    )
-                  : Hero(
-                      tag: product!.id,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16),
-                        child: CachedNetworkImage(
-                          fit: BoxFit.fill,
-                          imageUrl: product!.images.first,
-                          placeholder: (context, url) => Shimmer.fromColors(
-                            baseColor: Colors.grey.shade100,
-                            highlightColor: Colors.white,
-                            child: Container(),
-                          ),
-                          errorWidget: (context, url, error) =>
-                              const Center(child: Icon(Icons.error)),
+                    ],
+                    border: Border.all(
+                      color: Theme.of(context).shadowColor.withOpacity(0.15),
+                    ),
+                  ),
+                  child: product == null
+                      ? Material(
+                    child: GridTile(
+                      footer: Container(),
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Container(
+                          color: Colors.grey.shade300,
                         ),
                       ),
                     ),
-            )),
-            Padding(
+                  )
+                      : Hero(
+                    tag: product!.id,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: CachedNetworkImage(
+                        fit: BoxFit.cover,
+                        imageUrl: product!.images.first,
+                        placeholder: (context, url) =>
+                            Shimmer.fromColors(
+                              baseColor: Colors.grey.shade100,
+                              highlightColor: Colors.white,
+                              child: Container(),
+                            ),
+                        errorWidget: (context, url, error) =>
+                        const Center(child: Icon(Icons.error)),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
                 padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
                 child: SizedBox(
-
                   child: product == null
                       ? Container(
-                          width: 120,
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        )
-                      : Text(
-                          product!.name,
-                          style: const TextStyle(fontWeight: FontWeight.w600),
-                        ),
-                )),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
-              child: Row(
-                children: [
-                  SizedBox(
-                    child: product == null
-                        ? Container(
-                            width: 100,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          )
-                        : Text(
-                            '₹' + product!.priceTags.first.price.toString(),
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
+                    width: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   )
-                ],
+                      : Text(
+                    product!.name,
+                    style: const TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      child: product == null
+                          ? Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      )
+                          : Text(
+                        '₹' + product!.priceTags.first.price.toString(),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
